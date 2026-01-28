259290674b5e7baae9885df5f6e0d8332b0e6530
94q <80714938+94q@users.noreply.github.com>
2026-01-29 00:04:39 +0200

    t

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
@@ -8,7 +7,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  <HashRouter>
-  <App />
-</HashRouter>,
 )
